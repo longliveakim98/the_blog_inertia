3b27ed777f3900226eca0382625c4f8fd511a2ea
longliveakim98 <mohdaimanhakim.98@gmail.com>
2024-12-19 14:16:24 +0800

    rename

--- resources/js/Components/AuthorDate.jsx
+++ resources/js/Components/AuthorDate.jsx
@@ -1,5 +1,5 @@
 import { Link } from "@inertiajs/react";
-import { useRoute } from "../../../vendor/tightenco/ziggy";
+import { useRoute } from "../../../vendor/tightenco/ziggy/src/js";
 import { formatDate } from "./formatDate";
 export default function AuthorDate({ author, date, userId }) {
     const route = useRoute();
